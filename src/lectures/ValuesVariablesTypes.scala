f6a43ec32b3d3df2ebb7682cae2a45fc7197bff6
bodhibrdr <julie.gumerman@gmail.com>
2020-06-22 22:19:17 -0600

    scala primitive data structures

--- src/lectures/ValuesVariablesTypes.scala
+++ src/lectures/ValuesVariablesTypes.scala
@@ -3,8 +3,28 @@ package lectures
 object ValuesVariablesTypes extends App {
 
     //vals are immutable!!!
+    //vals' types of value is optional
+    //compiler can infer types
 
+  //Values
     val x: Int = 37
     println(x)
+
+    val aString: String = "haiiiii"
+    println(aString)
+    val pupper: String = "Reverie!!!"
+
+    val pupIsGood: Boolean = true
+    val charact: Char = 'd'
+    val anInt: Int = 22
+    val shortie: Short = 2002
+    val longo: Long = 20021492
+    val piMe: Float = 3.1415f
+    val aDouble: Double = 3.14
+
+    //Variables
+    var aVariable: Int = 4
+
+
 
 }
