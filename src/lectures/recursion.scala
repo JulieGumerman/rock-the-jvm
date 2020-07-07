d0ad152403f539655752638d3084e797f801393a
bodhibrdr <julie.gumerman@gmail.com>
2020-07-06 21:33:43 -0600

    a little bit of recursion

--- src/lectures/recursion.scala
+++ src/lectures/recursion.scala
@@ -15,4 +15,21 @@ object recursion extends App {
     factHelper(n, 1)
   }
 
+
+  def concatenateTailRec(aString: String, n: Int, accumulator: String): String =
+    if (n <=0 ) accumulator
+    else concatenateTailRec(aString, n-1, aString + accumulator)
+
+  println(concatenateTailRec("Yeet", 3, ""))
+
+  def isPrime(n: Int): Boolean = {
+    def isPrimeTailRec(t: Int, isStillPrime: Boolean): Boolean = {
+      if (!isStillPrime) false
+      if (t <= 1) true
+      else isPrimeTailRec(t - 1, n % t != 0 )
+
+      isPrimeTailRec(n/2, true)
+    }
+
+  }
 }
