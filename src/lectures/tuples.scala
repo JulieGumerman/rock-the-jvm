5705c6e7f6366613dabddc2e6330eb37cf31a8ed
bodhibrdr <julie.gumerman@gmail.com>
2020-07-13 19:41:27 -0600

    more fun w tuples

--- src/lectures/tuples.scala
+++ src/lectures/tuples.scala
@@ -2,7 +2,8 @@ package lectures
 
 object tuples extends App{
 
-  val personInfo("Julie", "Gumerman", 37, "F")
-  println(personInfo)
+  val personInfo =("Julie", "Gumerman", 37, "F")
+  val skating = ("figure skating", "rollerskating", "snowboarding", "longboarding", "skateboarding")
+  skating.productIterator.foreach{i => println("Activity: " + i)}
 
 }
