7d253e58d0fea0977661c884fc3e708d1389f3bd
bodhibrdr <julie.gumerman@gmail.com>
2020-07-13 19:35:05 -0600

    tuplesgit add . !

--- src/lectures/PatternMatching.scala
+++ src/lectures/PatternMatching.scala
@@ -23,6 +23,25 @@ object PatternMatching extends App {
     case "Friday" => "Ice skating day!!!"
   }
 
-  println(typeOfDay)
 
+  val innerGoddessActivity = "face mask and read"
+
+  val goddessActivityLog = innerGoddessActivity match {
+    case "figure skating" | "roller skating" => "You are strong and graceful"
+    case "rock climbing" =>  "Look at how you use your body to make it happen!!!"
+    case "longboarding" | "snowboarding" => "How powerful yet how dainty"
+    case "yoga" | "belly dance" => "Yeah, stretch and dance off the angst!!!"
+    case _ => "Whatever you do, you are freaking awesome. Rock on, goddess!!!"
+
+  }
+
+
+  val meaningOfLife:Any = 42
+
+  val typeOfMeaning = meaningOfLife match {
+    case meaningOfLife:Int => "Because math is life"
+    case meaningOfLife:String => "That's deep man."
+    case meaningOfLife:Double => "It never can be easy"
+    case _ => "Go with the flow, my dudes."
+  }
 }
---
+++ src/lectures/tuples.scala
@@ -0,0 +1,8 @@
+package lectures
+
+object tuples extends App{
+
+  val personInfo("Julie", "Gumerman", 37, "F")
+  println(personInfo)
+
+}
