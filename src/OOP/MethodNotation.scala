d685c7669ef50d56a52ea590533104478c49ee5d
bodhibrdr <julie.gumerman@gmail.com>
2020-07-17 21:49:20 -0600

    method notation

--- src/OOP/MethodNotation.scala
+++ src/OOP/MethodNotation.scala
@@ -4,10 +4,15 @@ object MethodNotation extends App {
 
   class Person (val name: String, favoriteMovie: String) {
     def likes(movie: String): Boolean = movie == favoriteMovie
+    def hangOutWith(person: Person): String = s"${this.name} hangs out with ${person.name}"
   }
 
+  val bob = new Person("Bob", "The Little Mermaid")
   val mary = new Person("Mary", "EuroTrip")
+  
   println(mary.likes("EuroTrip"))
   println(mary likes "EuroTrip") //infix notation = operator notation
+  println(mary.hangOutWith(bob))
+
 
 }
