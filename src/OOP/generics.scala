aec9e8162bf77326344938d8164d22aa651819ba
bodhibrdr <julie.gumerman@gmail.com>
2020-07-26 15:50:17 -0600

    messing around with traits and syntactic sugar

--- src/OOP/generics.scala
+++ src/OOP/generics.scala
@@ -3,7 +3,9 @@ package OOP
 object generics extends App {
 
   abstract class Climber {
-    def climbs: Unit
+    def climbs: Unit = {
+      println("We love sandstone!")
+    }
   }
 
 
@@ -11,5 +13,18 @@ object generics extends App {
     override def climbs: Unit = println("Is it winter yet?")
   }
 
+  println(iceClimber climbs)
+
+  trait femaleClimber {
+    def pees: Unit = println("Never underestimate the trouble of peeing in a climbing harness.")
+  }
+
+  class Julie extends Climber with femaleClimber {
+
+  }
+
+  val julie = new Julie
+
+  julie.pees
   //this is equivalent to declaring a class IceClimber which extends Climber and overrides climbs
 }
